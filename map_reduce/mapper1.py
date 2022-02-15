6e52946c6fd57ae4271ffe7a6a782ffd7bba81f2
mangaioct6 <90936170+mangaioct6@users.noreply.github.com>
2022-02-15 09:41:54 -0600

    Delete mapper1.py

--- map_reduce/mapper1.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# mapper - we are going to break the sentences into tokens or units
-
-import sys
-
-# stdin - standard input
-for line in sys.stdin:
-    # strip white spaces at the beggining and end of line
-   print(line)
